6053c05b8a3b13f8eb0ccbbc5b4b7a478c6eebf1
pkhamdee <pkhamdee@gmail.com>
2020-05-25 13:34:41 +0700

    fix bug

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,8 @@ namespace seroter_api_k8s
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
